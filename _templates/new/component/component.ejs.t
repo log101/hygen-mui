8f924ed65c94f3ab03bd3c458fe4354e819afe7e
Rich Sturim <rich.sturim@coxautoinc.com>
2021-10-01 10:38:36 -0400

    remove js specific code

--- _templates/new/component/component.ejs.t
+++ _templates/new/component/component.ejs.t
@@ -4,26 +4,6 @@ to:  <%= locals.path %>/<%= h.inflection.camelize(name, false) %>/<%= h.inflecti
 <% componentName = h.inflection.camelize(name, false) -%>
 <% klassName = h.inflection.camelize(componentName, true) -%>
 <% cssClassName = h.changeCase.paramCase(componentName, true) -%>
-
-<% if (locals.type === 'functional') { -%>
-import React from 'react';
-
-<% if(locals.includeCss){ -%>
-import './<%= klassName %>.scss';
-<% } -%>
-
-const <%= componentName %> = () => (
-<%_ if(locals.includeCss){ _%>
-    <div data-testid="<%= cssClassName %>-component" className="<%= cssClassName %>-container">
-<% } else{ _%>
-    <div>
-<% } _%>
-        <h1><%= componentName %> Component</h1>
-    </div>
-);
-
-export default <%= componentName %>;
-<% } -%>
 <% if (locals.type === 'functional-typescript') { -%>
 import React from 'react';
 
@@ -74,55 +54,3 @@ class <%= componentName %> extends Component<Props, State> {
 export default <%= componentName %>;
 <% } -%>
 
-<% if (locals.type === 'stateful') { -%>
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-<% if(locals.includeCss){ -%>
-import './<%= klassName %>.scss';
-<% } -%>
-
-export class <%= componentName %> extends Component {
-    static propTypes = {};
-
-    state = {
-        foo: 'bar',
-    };
-
-    render() {
-        return (
-<%_ if(locals.includeCss){ _%>
-            <div className="<%= cssClassName %>-container">
-<% } else{ _%>
-            <div>
-<% } _%>
-                <h1><%= componentName %>, the start of a React Stateful Component</h1>
-            </div>
-        );
-    }
-}
-
-export default <%= componentName %>;
-<% } -%>
-<% if (locals.type === 'pure') { -%>
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-<% if(locals.includeCss){ -%>
-import './<%= klassName %>.scss';
-<% } -%>
-
-export default class <%= componentName %> extends PureComponent {
-    static propTypes = {};
-
-    render() {
-        return (
-<%_ if(locals.includeCss){ _%>
-            <div className="<%= cssClassName %>-container">
-<% } else{ _%>
-            <div>
-<% } _%>
-                <h1><%= componentName %>, the start of a React Pure Component</h1>
-            </div>
-        );
-    }
-}
-<% } -%>
--- _templates/new/component/prompt.js
+++ _templates/new/component/prompt.js
@@ -2,72 +2,59 @@ module.exports = {
     prompt: ({ inquirer }) => {
         const questions = [
             {
-                type: 'input',
-                name: 'name',
-                message: 'What is the component name?',
+                type: "input",
+                name: "name",
+                message: "What is the component name?",
             },
             {
-                type: 'select',
-                name: 'type',
-                message: 'Choose the type of your component',
+                type: "select",
+                name: "type",
+                message: "Choose the type of your component",
                 choices: [
                     {
-                        message: 'TypeScript - React Arrow Function Component',
-                        value: 'functional-typescript',
+                        message: "TypeScript - React Arrow Function Component",
+                        value: "functional-typescript",
                     },
                     {
-                        message: 'JavaScript - React Arrow Function Component',
-                        value: 'functional',
-                    },
-                    {
-                        message: 'TypeScript - Stateful Component',
-                        value: 'stateful-typescript',
-                    },
-                    {
-                        message: 'JavaScript - Stateful Component',
-                        value: 'stateful',
+                        message: "TypeScript - Stateful Component",
+                        value: "stateful-typescript",
                     },
                 ],
             },
             {
-                type: 'select',
-                name: 'path',
-                message: 'Choose a destination for you component',
+                type: "select",
+                name: "path",
+                message: "Choose a destination for you component",
                 choices: [
                     {
-                        message: 'App components',
-                        value: './src/components',
+                        message: "App components",
+                        value: "./src/components",
                     },
                     {
-                        message: 'UI components',
-                        value: './src/components/ui',
+                        message: "UI components",
+                        value: "./src/components/ui",
                     },
                 ],
             },
             {
-                type: 'toggle',
-                message: 'Do you want to include a test file?',
-                name: 'includeTest',
-                enabled: 'Heck Yeah',
-                disabled: 'Nope',
+                type: "toggle",
+                message: "Do you want to include a test file?",
+                name: "includeTest",
+                enabled: "Heck Yeah",
+                disabled: "Nope",
             },
             {
-                type: 'toggle',
-                message: 'Do you want to include an imported style sheet?',
-                name: 'includeCss',
-                enabled: 'For sure',
-                disabled: 'Nope',
+                type: "toggle",
+                message: "Do you want to include an imported style sheet?",
+                name: "includeCss",
+                enabled: "For sure",
+                disabled: "Nope",
             },
         ];
-        return inquirer.prompt(questions).then(answers => {
+        return inquirer.prompt(questions).then((answers) => {
             const { type } = answers;
-            let extension = 'js';
-            let componentExt = 'jsx';
-
-            if (type.indexOf('typescript') > -1) {
-                extension = 'ts';
-                componentExt = 'tsx';
-            }
+            let extension = "ts";
+            let componentExt = "tsx";
 
             return { ...answers, extension, componentExt };
         });
